21d9e938d85b065ad18ca8e9d81b48286ed32f0c
Nokk3n <66500104+nokk3n-dev@users.noreply.github.com>
2025-04-17 18:25:28 -0700

    Added safe and key

--- Assets/Scripts/Puzzles/SafePuzzle.cs
+++ Assets/Scripts/Puzzles/SafePuzzle.cs
@@ -1,31 +1,80 @@
 using UnityEngine;
 using Unity.Netcode;
+using System.Collections.Generic;
 
-public class SafePuzzle : NetworkBehaviour, IInteractable
+public class SafePuzzle : NetworkBehaviour, IInteractable, IHasNetworkChildren
 {
-    private Transform safeTransform;
-    [SerializeField] private GameObject safePrefab;
-    [SerializeField] private Animator safeAnimator;
-    [SerializeField] private GameObject keyParent;
-    private bool keyUnlocked;
-    private bool isLocked;
-    private bool isOpen;
+    private Animator safeAnimator;
+    private GameObject key;
+    [SerializeField] private GameObject safeCollider;
+    [SerializeField] private Transform keyTransform;
+    [SerializeField] private ItemScriptableObject itemScriptableObjectToSpawn;
+    private InteractableItemBase keyScript;
+    private float interactCooldown = 1f;
+    private float interactCooldownTimer;
+
+    [Header("Network Variables")]
+    private NetworkVariable<bool> isOpen = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private NetworkVariable<bool> isLocked = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private NetworkVariable<bool> canInteract = new NetworkVariable<bool>(true, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isOpen = false;
-        isLocked = false;
-        keyUnlocked = false;
         safeAnimator = GetComponentInChildren<Animator>();
     }
 
-    [ServerRpc(RequireOwnership = false)]
+    void Update()
+    {
+        if (IsServer)
+        {
+            HandleInteractCooldown();
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (IsServer)
+        {
+           
+        }
+    }
+
+    private void SpawnKey()
+    {
+         key = Instantiate(itemScriptableObjectToSpawn.physicalItemPrefab, keyTransform.position, keyTransform.rotation);
+            key.GetComponent<NetworkObject>().Spawn(true);
+            keyScript = key.GetComponent<InteractableItemBase>();
+            keyScript.isStored.Value = true;
+    }
+
+    public void DestroyNetworkChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            NetworkObject childNetObj = child.GetComponent<NetworkObject>();
+            if (childNetObj != null)
+            {
+                if (childNetObj.IsSpawned)
+                {
+                    if(childNetObj.GetComponent<IHasNetworkChildren>() != null)
+                    {
+                        childNetObj.GetComponent<IHasNetworkChildren>().DestroyNetworkChildren();
+                    }
+                    childNetObj.Despawn(true);
+                }
+            }
+        }
+    }
+
+    [Rpc(SendTo.Server)]
     private void OpenSafeServerRpc()
     {
+        interactCooldownTimer = interactCooldown;
         OpenSafeClientRpc();
     }
 
-    [ClientRpc]
+    [Rpc(SendTo.Everyone)]
     private void OpenSafeClientRpc()
     {
         OpenSafe();
@@ -34,10 +83,15 @@ public class SafePuzzle : NetworkBehaviour, IInteractable
     private void OpenSafe()
     {
         Debug.Log("Opening Safe");
-        isOpen = true;
+        isOpen.Value = true;
+        //safeCollider.GetComponent<BoxCollider>().enabled = false;
         if (safeAnimator != null)
         {
             safeAnimator.SetTrigger("open");
+            if (IsServer)
+            {
+                SpawnKey();
+            }
         }
         else
         {
@@ -45,34 +99,28 @@ public class SafePuzzle : NetworkBehaviour, IInteractable
         }
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    private void GetKeyServerRpc()
+    private void HandleInteractCooldown()
     {
-        GetKeyClientRpc();
-    }
+        if (interactCooldownTimer > 0)
+        {
+            canInteract.Value = false;
+            interactCooldownTimer -= Time.deltaTime;
+        }
 
-    [ClientRpc]
-    private void GetKeyClientRpc()
-    {
-        GetKey();
-    }
-
-    private void GetKey()
-    {
-        Debug.Log("Key Obtained");
-        keyParent.SetActive(false);
-        keyUnlocked = true;
+        if (interactCooldownTimer <= 0)
+        {
+            canInteract.Value = true;
+        }
     }
 
     public void Interact(NetworkObjectReference playerObject)
     {
-        if (!isOpen && !isLocked)
+        if (!isOpen.Value && !isLocked.Value)
         {
-            OpenSafeServerRpc();
-        } 
-        else if (isOpen && !keyUnlocked)
-        {
-            GetKeyServerRpc();
+            if(canInteract.Value)
+            {
+                OpenSafeServerRpc();
+            }
         }
     }
 }
